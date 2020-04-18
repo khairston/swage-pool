7860edfd41b996e24c6f8cc6d46de911c449abd1
Katie Hairston <kth5sf@virginia.edu>
2020-04-18 11:47:46 -0400

    initial commit

---
+++ src/main/java/dev/hairston/swagepool/SwagePoolApplication.java
@@ -0,0 +1,13 @@
+package dev.hairston.swagepool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwagePoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwagePoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/hairston/swagepool/SwagePoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hairston.swagepool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwagePoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
